1b616c281cc8cc321ab0ba5e6db108da87c2b684
manishsaraan <manish021js@gmail.com>
2018-10-23 21:35:08 +0530

    save use repos implemented

--- server.js
+++ server.js
@@ -3,16 +3,18 @@ const passport = require('passport');
 const Strategy = require('passport-github').Strategy;
 const got = require("got");
 const mongoose = require('mongoose');
+const async = require("async");
 const baseUrl = "https://api.github.com";
 mongoose.connect('mongodb://node_passport:Admin123#@ds245523.mlab.com:45523/repos',{ useNewUrlParser: true });
 const Repos = require('./repos');
+const UserRepos = require('./user-repos');
 const clientID = "b2464a59102ba2db9cb1";
 const clientSecret = "4e265ceaa7bfc09315438f9d9e3d795302bca1ce";
 
 passport.use(new Strategy({
     clientID,
     clientSecret,
-    callbackURL: 'http://localhost:3000/login/github/return'
+    callbackURL: '/login/github/return'
   },
   function(accessToken, refreshToken, profile, cb) {
     console.log(accessToken, refreshToken, profile);
@@ -71,7 +73,7 @@ app.get('/login/github',
 app.get('/login/github/return', 
   passport.authenticate('github', { failureRedirect: '/login' }),
   function(req, res) {
-    res.redirect('/');
+    res.redirect('/profile');
   });
 
 
@@ -80,10 +82,10 @@ app.get('/profile',
   async (req, res) => {
     const { accessToken, username, _json: { public_repos } } = req.user;
     // const {body: sss} = await got(`${baseUrl}/users/sindresorhus?client_id=${clientID}&client_secret=${clientSecret}`, {json: true, method: 'GET'});
-    // console.log('*******************************s*********************',sss);
-    const totalPages = Math.ceil(992/100);
+
+    const totalPages = Math.ceil(public_repos/100);
     let reposData = [];
-    const apiUrl = `${baseUrl}/users/sindresorhus/repos?client_id=${clientID}&client_secret=${clientSecret}&per_page=100&page=1`;
+    const apiUrl = `${baseUrl}/users/${username}/repos?client_id=${clientID}&client_secret=${clientSecret}&per_page=100&page=1`;
     console.log(public_repos,"((repos(((((((((sddddddddddddd", apiUrl);
     const {body: repos} = await got(apiUrl, {json: true, method: 'GET'});
     console.log(username, "((((((((((((((repos(((((((((((((((((", repos.length);
@@ -91,14 +93,47 @@ app.get('/profile',
     reposData = [ ...reposData, ...repos];
     if(totalPages > 1){
     for(let i = 1; i<=totalPages; i++){      
-      let apiUrlNew = `${baseUrl}/users/sindresorhus/repos?client_id=${clientID}&client_secret=${clientSecret}&per_page=100&page=${i}`;
+      let apiUrlNew = `${baseUrl}/users/${username}/repos?client_id=${clientID}&client_secret=${clientSecret}&per_page=100&page=${i}`;
       console.log("((repos(((((((((apiUrlNew", apiUrlNew);
       const {body: repos} = await got(apiUrlNew, {json: true, method: 'GET'});
       console.log(username, "((((((((((((((repos(((((((((((((((((", repos.length);
       reposData = [ ...reposData, ...repos];      
     }
   }
-    res.render('profile', { user: req.user, repos: reposData });
+
+
+       async.mapSeries(reposData, (repo, cb) => {
+        const { full_name, stargazers_count, watchers_count, open_issues_count, created_at, forks_count, 
+          description, html_url, language, owner, id } = repo;
+        const { html_url: userProfileUrl, avatar_url, login } = owner;
+        
+        UserRepos.findOneAndUpdate({ github_id: id}, {
+          name: full_name,
+          github_id: id,
+          stargazers_count,
+          watchers_count,
+          open_issues_count,
+          forks_count,
+          description,
+          html_url,
+          language,
+          owner: {
+            userProfileUrl,
+            avatar_url,
+            username: login
+          },
+          created_at
+        }, { upsert: true, new: true, setDefaultsOnInsert: true }, function(error, savedData) {
+          if (error) return;
+          cb(null, savedData);
+          // do something with the document
+        });
+       },(err, repos) => {
+         console.log(repos)
+         res.render('profile', { user: req.user, repos: repos });
+
+       });
+
   });
 
   app.get('/publish/:repoId', async (req, res) => {
@@ -110,33 +145,7 @@ app.get('/profile',
     const apiUrl = `${baseUrl}/repos/${username}/${repoId}?token=${accessToken}`;
     console.log("((repos(((((((((sddddddddddddd", apiUrl);
     const {body: repoContent} = await got(apiUrl, {json: true, method: 'GET'});
-    const { full_name, stargazers_count, watchers_count, open_issues_count, created_at, forks_count, 
-      description, html_url, language, owner} = repoContent;
-    const { html_url: userProfileUrl, avatar_url, login } = owner;
-    console.log(full_name, watchers_count, "((((((((((((((repo data(((((((((((((((((", owner);
-    var repo = new Repos({
-      name: full_name,
-      stargazers_count,
-      watchers_count,
-      open_issues_count,
-      forks_count,
-      description,
-      html_url,
-      language,
-      owner: {
-        userProfileUrl,
-        avatar_url,
-        username: login
-      },
-      created_at
-    });
 
-    repo.save(function(err, savedData) {
-      if (err) throw err;
-      console.log('User saved successfully!',savedData);
-
-      res.json(savedData);
-    });
     
   });
 
