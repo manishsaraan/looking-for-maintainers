c1939464dba0099f88fd4dd41d482c0dc481da0d
manishsaraan <manish021js@gmail.com>
2018-10-30 20:07:48 +0530

    global css file added

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
-
+app.use(express.static('assets'))
 // Initialize Passport and restore authentication state, if any, from the
 // session.
 app.use(passport.initialize());
--- user-repos.js
+++ user-repos.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
+
 const userReposSchema = new Schema({
     github_id: { type: Number, required: true },
     userId: { type: Schema.Types.ObjectId, ref: "Users", required: true },
