21b8deaa65ba2edf32a56903af9a5656f50b5c00
manishsaraan <manish021js@gmail.com>
2020-04-06 16:04:14 +0530

    minor changes in config

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,5 +1,6 @@
-export const clientId = "13f5b2407b9f1ff4657b";
-export const redirectUri =
+export const clientId: string | undefined = process.env.REACT_APP_CLIENT_ID;
+export const redirectUri: string =
   process.env.REACT_APP_REDIRECT_URI || "/login/github/return";
-export const apiEndPoint = "http://localhost:3002";
-export const gaKey = ""
+export const apiEndPoint: string =
+  process.env.REACT_APP_API_ENDPOINT || "http://localhost:3002";
+export const gaKey: string | undefined = process.env.REACT_APP_GA_KEY;
