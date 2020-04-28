d7ecacb41ecfff14b4a4ca0ebdcde3825662339f
Manish Saraan <manish021js@gmail.com>
2020-04-28 21:51:41 +0530

    config updated

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -3,4 +3,4 @@ export const redirectUri: string =
   process.env.REACT_APP_REDIRECT_URI || "/login/github/return";
 export const apiEndPoint: string =
   process.env.REACT_APP_API_ENDPOINT || "http://localhost:3002";
-export const gaKey: string | undefined = process.env.REACT_APP_GA_KEY;
+export const gaKey = process.env.REACT_APP_GA_KEY;
